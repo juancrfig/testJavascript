00ab68e6d356156980333f2d479ef7a595bf89a6
juancrfig <juancrfig@gmail.com>
2025-01-23 21:02:44 -0500

    Improve loggin functionality

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Kanban Board for Momentum
+
+I decided to start designing the website as mobile first to take advantage of the natural
+flow of the HTML elements, so I can save time. However, I noticed the importance of using
+frameworks which could have saved me a lot more of time in this exam. 
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,31 @@
+// DEFINE VARIABLES FOR SIGN UP AND LOG IN
+
+const navUlElement = document.querySelector('nav ul');
 const containerModalLogInElm = document.querySelector('.container-modal-login');
 const logInBtn = document.querySelector('.log-in-button');
 const signUpBtn = document.querySelector('.sign-up-button');
-const logInBtnSubmit = document.querySelector('.submit-log-in');
+const logInBtnSubmit = document.querySelector('#submit-log-in');
 const closeLogIn = document.querySelector('.modal-log-in i');
 const containerModalSignUpElm = document.querySelector('.container-modal-sign-up');
 const closeSignup = document.querySelector('.modal-sign-up i');
+const emailInputLogIn = document.querySelector('#email-log-in');
+const passInputLogIn = document.querySelector('#pass-log-in');
+const emailInputSignUp = document.querySelector('#email-sign-up');
+const passInputSignUp = document.querySelector('#pass-sign-up');
+const signUpBtnSubmit = document.querySelector('#submit-sign-up');
 
+
+
+// EVENT LISTENER FOR CHECKING IF THERE'S AN ACTIVE SESSION
+
+document.addEventListener('DOMContentLoaded', () => {
+    const localItem = localStorage.getItem('sessionStatus'); 
+    if (localItem) {
+        loadUserData(localItem);
+    }
+})
+
+// ADD EVENT LISTENER DIRECTLY IN DOCUMENT FOR EFFICIENCY AND READABILITY
 
 document.addEventListener('click', (e) => {
 
@@ -13,11 +33,17 @@ document.addEventListener('click', (e) => {
         case logInBtn:
             toggleModal(containerModalLogInElm);
             break;
+        case logInBtnSubmit:
+            logIn(emailInputLogIn.value, passInputLogIn.value);
+            break;
         case closeLogIn:
             toggleModal(containerModalLogInElm);
             break;
         case signUpBtn:
             toggleModal(containerModalSignUpElm);
+            break;
+        case signUpBtnSubmit:
+            signUp(emailInputSignUp.value, passInputSignUp.value);
             break;
         case closeSignup:
             toggleModal(containerModalSignUpElm);
@@ -32,4 +58,37 @@ document.addEventListener('click', (e) => {
 function toggleModal(modalElement) {
     modalElement.classList.toggle('disabled');
     modalElement.classList.toggle('center-flex');
+}
+
+
+// LOGIC FOR SIGNING UP AND LOGGING IN
+
+function logIn(email, password) {
+    let userData = localStorage.getItem(email);
+    userData = JSON.parse(userData)
+    if (userData[0] === password) {
+        userData[2] = true;
+        localStorage.setItem(email, JSON.stringify(userData));
+        localStorage.setItem('sessionStatus', email)
+        alert('Log In Success!');
+    } else {
+        alert('Incorrect data!');
+    }
+}
+
+function signUp(email, password) {
+    const tasks = [];
+    const userData = [password, tasks, session];
+    localStorage.setItem(email, JSON.stringify(userData));
+}
+
+
+// LOGIC FOR LOADING USER'S DATA
+
+function loadUserData(email) {
+    
+    navUlElement.innerHTML = '';
+    navUlElement.textContent = 'Welcome';
+
+    
 }
